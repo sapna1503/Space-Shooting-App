a3210cb29d6b915fc73a42480f72a5fcc241d23e
Sapna Chandiramani <chandiramanisapna15@gmail.com>
2019-02-16 15:07:19 -0800

    commit

---
+++ app/src/main/java/com/example/schandiramani/spaceshooter/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.schandiramani.spaceshooter
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun startGame(view: View) {
+        val intent = Intent(this, StartGameActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+}
